712f40ada027f11c607afc3c6c14d3f06db4c9b3
bratelefant <59534030+bratelefant@users.noreply.github.com>
2023-12-30 14:04:26 +0100

    updated package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Provide a simple way to manage API keys, express middleware included.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/bratelefant/meteor-api-keys',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md',
